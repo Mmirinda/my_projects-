c5b5d00bdfc22c38585e8956dba266e12afe5f6d
Yury Shamanov <67365128+Eytes@users.noreply.github.com>
2023-11-28 19:10:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ import controllers
 
 contact_controller = controllers.ContactsController()
 contact_controller.get_contact_by_phone_number("+79114563843")
-
-contact_controller.get_contact(vasia=123)
